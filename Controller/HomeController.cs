a8a2d25a72992ff99b191ee605672f0ee8f1cb1e
Melvin <83341188+Mvchew@users.noreply.github.com>
2023-03-28 14:25:17 -0600

    Rearranging File Structure

---
+++ Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NapMap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Model/MapLocation.cs
@@ -0,0 +1,7 @@
+﻿namespace NapMap.Model
+{
+    public class MapLocation
+    {
+        //TODO
+    }
+}
