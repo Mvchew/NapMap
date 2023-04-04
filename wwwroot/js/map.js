0f5bf070457f23fd17c389ddee7936c24b697829
Melvin <83341188+Mvchew@users.noreply.github.com>
2023-04-04 17:31:21 -0600

    map.js for map client side functions

---
+++ wwwroot/js/map.js
@@ -0,0 +1,31 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const map = L.map('map').setView([51.0486, -114.0708], 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var marker = L.marker([51.0486, -114.0708], {
+        title: "City of Calgary",
+        draggable: true,
+        properties: {
+            population: 1392609,
+            website: "https://www.calgary.ca",
+            industries: [
+                {
+                    name: "Energy"
+                },
+                {
+                    name: "Technology"
+                },
+                {
+                    name: "Financial Services"
+                }
+            ]
+        }
+    }).addTo(map);
+
+    marker.bindPopup("<b>" + marker.options.title + "</b><br>" +
+        "Top industries: " + marker.options.properties.industries.map(i => i.name).join(", ") + "<br>"
+    );
+});
